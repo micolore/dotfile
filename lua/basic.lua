f2cf6856d47d66b98c493bdd8cd79ea958a156e4
kubrick <micolore2013@gmail.com>
2024-09-05 17:41:29 +0800

    update code

--- lua/basic.lua
+++ lua/basic.lua
@@ -371,7 +371,7 @@ vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
 vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
 vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
 vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
-vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
+vim.keymap.set('n', '<C-//>', require('smart-splits').move_cursor_previous)
 -- swapping buffers between windows
 vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
 vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
