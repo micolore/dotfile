cfce68228b923b390347d60c87ab5d4b1125ea59
kubrick <471304402@qq.com>
2024-12-30 11:59:56 +0800

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -1,13 +1,19 @@
 # download config file 
 
+mkdir -p ~/.config/nvim
+
 wegt https://github.com/micolore/dotfile/archive/refs/heads/main.zip
 
-unzip -o /tmp/dotfile.zip -d $TARGET_DIR
+unzip -o main.zip -d ~/.config/nvim
 
-mkdir -p ~/.config/nvim
+cd ~/.config/nvim 
+
+mv dotfile-main/* .
 
 # install neovim 
 
 yum install -y neovim
+
+# neovim upgrade not support  must compile 
 
 # config neovim 
