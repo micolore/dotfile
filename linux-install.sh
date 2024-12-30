8037b06621d73ff9d5eb2806680239fc647256e5
kubrick <471304402@qq.com>
2024-12-30 11:00:06 +0800

    Create linux-install.sh

---
+++ linux-install.sh
@@ -0,0 +1,13 @@
+# download config file 
+
+wegt https://github.com/micolore/dotfile/archive/refs/heads/main.zip
+
+unzip -o /tmp/dotfile.zip -d $TARGET_DIR
+
+mkdir -p ~/.config/nvim
+
+# install neovim 
+
+yum install -y neovim
+
+# config neovim 
